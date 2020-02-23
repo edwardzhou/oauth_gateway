346c13ceaef107cccaeb24fc4e332d414bcfcfcf
Edward Zhou <edwarz.zhou@scrumcn.com>
2020-02-23 14:54:18 +0800

    implements find_authentication and test case

--- oauth_gateway_app/apps/oauth_gateway/test/oauth_gateway/accounts_test.exs
+++ oauth_gateway_app/apps/oauth_gateway/test/oauth_gateway/accounts_test.exs
@@ -2,4 +2,24 @@ defmodule OauthGateway.AccountsTest do
   use OauthGateway.DataCase
 
   alias OauthGateway.Accounts
+
+  alias Accounts.Authentication
+
+  alias OauthGateway.ModelFactory
+
+  describe "Accounts#find_authentication" do
+    @auth_id UUID.uuid4()
+    @uid "123"
+
+    test "should returns {:ok, authentication} with exists uid" do
+      new_auth = ModelFactory.insert(:authentication, %{uid: @uid})
+      {:ok, %Authentication{} = auth} = Accounts.find_authentication(@uid)
+
+      assert new_auth == auth
+    end
+
+    test "should returns {:not_found, nil} with non-exist uid" do
+      {:not_found, nil} = Accounts.find_authentication(@uid)      
+    end
+  end
 end
