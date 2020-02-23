982602a0b60a716c2eb13f9fd1fd08855950272c
Edward Zhou <edwarz.zhou@scrumcn.com>
2020-02-23 12:58:51 +0800

    add test/support to dev mode for ease of interative development

--- oauth_gateway_app/apps/oauth_gateway/mix.exs
+++ oauth_gateway_app/apps/oauth_gateway/mix.exs
@@ -29,6 +29,7 @@ defmodule OauthGateway.MixProject do
 
   # Specifies which paths to compile per environment.
   defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(:dev), do: ["lib", "test/support"]
   defp elixirc_paths(_), do: ["lib"]
 
   # Specifies your project dependencies.
